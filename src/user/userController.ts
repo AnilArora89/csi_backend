3b89f69375339f783820f8f1db1e8cdd94513b49
varinder-008 <varetsingh88@gmail.com>
2024-06-22 12:17:30 +0530

    UserSchema and model created and also checked if already user exists with same email

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction , Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req : Request,res : Response , next : NextFunction) => {
     //when request comes follow few steps:
@@ -15,7 +16,14 @@ const createUser = async (req : Request,res : Response , next : NextFunction) =>
         // or we could have just returned return res.json({message:INVALID});
     }
     
+    //now we will check whether the user trying to registered is already registerd or not?
+    //database call
     
+    const user = await userModel.findOne({email: email});
+    if(user){
+        const error = createHttpError(400,"User Already Exists with this email");
+        return next(error);
+    }
     res.json({message:"MSG"});
 }
 
