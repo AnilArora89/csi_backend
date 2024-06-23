b472b3c68cb6232e190fd2e0c5334df3d214a61b
varinder-008 <varetsingh88@gmail.com>
2024-06-23 21:44:51 +0530

    Setup Multer (upload)

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,5 +1,8 @@
 import { Response , Request ,NextFunction } from "express";
-//import bookModel from './bookModel';
+import bookModel from './bookModel';
+//installed multer for raw-data types input
+//multer act as middleware
+//multer used in routes
 const createBook = async(req:Request , res:Response , next:NextFunction) => {
 
 }
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,7 +1,27 @@
 import exprees from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
+import path from "node:path";
 const bookRouter = exprees.Router();
 
-bookRouter.post("/register" , createBook);
+
+const upload = multer({
+    dest: path.resolve(__dirname, "../../public/data/uploads"),
+    limits: {fileSize: 3e7} //30 mb == 3e7
+});
+
+
+// bookRouter.post("/register" ,() => {} ,createBook); - used for custom middlewares so that before request goes to controller it can be worked upon
+bookRouter.post("/register", upload.fields([
+    {name: "coverImage", maxCount: 1},
+    {name: "file", maxCount: 1}
+]) , createBook);
 
 export default bookRouter;
+
+//Working of Multer
+
+
+/*Firstly multer creates a file in local machine then the file is sent to
+  cloud storage then it gets deleted from the local machine
+*/
