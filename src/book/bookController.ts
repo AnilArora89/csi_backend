ed957da36f24509fd93ff274168658877d94c7e8
varinder-008 <varetsingh88@gmail.com>
2024-06-24 11:50:47 +0530

    Created Book instance in db from cloud and then unlinked files from local storage

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,10 +2,12 @@ import { Response, Request, NextFunction } from "express";
 import bookModel from './bookModel';
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
+import fs from "node:fs"
 import createHttpError from "http-errors";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        const { title, genre } = req.body;
         console.log("files", req.files);
         const files = req.files as { [filename: string]: Express.Multer.File[] };
         const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
@@ -30,7 +32,24 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
 
         console.log("COVER UPLOAD RESULT", uploadResult)
         console.log("BOOKFILE UPLOAD RESULT", bookFileUploadResult)
-        res.json({});
+        //creation of book in db
+        const newBook = await bookModel.create({
+            title,
+            genre,
+            author: '',
+            coverImage: uploadResult.secure_url,
+            file: bookFileUploadResult.secure_url
+        })
+
+        try {
+            await fs.promises.unlink(filepath);
+            await fs.promises.unlink(Bookfilepath);
+        } catch (err) {
+            console.log(err);
+            next(createHttpError(500, "Error while deleting"))
+        }
+
+        res.status(201).json({ id: newBook._id });
     } catch (err) {
 
         console.log(err);
