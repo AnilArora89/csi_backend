9611cb80319b4dd7bb5ce7e2bdc03f6457e85e91
varinder-008 <varetsingh88@gmail.com>
2024-06-26 00:35:11 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,25 @@
-import express, { Request , Response ,NextFunction } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { HttpError } from "http-errors";
 import { config } from "./config/config";
+import cors from "cors";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
 const app = express();
+app.use(cors({
+    origin: "https://localhost:3000"
+}));
+
 app.use(express.json());
 //Routes setting
 
-app.get('/', (req,res,next) =>{
-    res.json({message : "WELCOME TO CSI"});
+app.get('/', (req, res, next) => {
+    res.json({ message: "WELCOME TO CSI" });
 })
 
 //user route setup
-app.use('/api/users' , userRouter);
-app.use('/api/books',bookRouter);
+app.use('/api/users', userRouter);
+app.use('/api/books', bookRouter);
 
 //global error handler ( to handle eroors in request handlers)
 app.use(globalErrorHandler);
