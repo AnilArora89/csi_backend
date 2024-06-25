ee26fd835c191fc28caa562b3c87cf9b2229b3c8
varinder-008 <varetsingh88@gmail.com>
2024-06-25 10:43:07 +0530

    Minor changes in authrequest

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,3 +1,4 @@
+
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import { verify } from "jsonwebtoken";
@@ -6,7 +7,7 @@ import { config } from "../config/config";
 export interface AuthRequest extends Request {
   userId: string;
 }
-const authenticate = (req: Request, res: Response, next: NextFunction) => {
+const authenticate = (req: AuthRequest, res: Response, next: NextFunction) => {
   const token = req.header("Authorization");
   if (!token) {
     return next(createHttpError(401, "Authorization token is required."));
@@ -15,8 +16,7 @@ const authenticate = (req: Request, res: Response, next: NextFunction) => {
   try {
     const parsedToken = token.split(" ")[1];
     const decoded = verify(parsedToken, config.jwtSecret as string);
-    const _req = req as AuthRequest;
-    _req.userId = decoded.sub as string;
+    req.userId = decoded.sub as string;
 
     next();
   } catch (err) {
